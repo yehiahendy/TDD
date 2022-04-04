3274671b8f4f9a3f94d8b3c710cf0e223b14b3e8
yehiahendy <yayahendy11@gmail.com>
2022-04-04 14:50:24 +0200

    cover when there's data useCase

--- webService/src/test/java/com/example/webservice/service/UserServiceTest.java
+++ webService/src/test/java/com/example/webservice/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.webservice.service;
 import com.example.webservice.model.User;
 import com.example.webservice.repository.UserRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,11 +19,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
+@DisplayName("User Service Test Class")
 public class UserServiceTest {
     @MockBean
     private UserRepository userRepository;
     @Autowired
     private UserService userService;
+
+
 
     @TestConfiguration
     static class UserServiceContextConfiguration {
@@ -30,8 +35,8 @@ public class UserServiceTest {
             return new UserService();
         }
     }
-
     @Test
+    @DisplayName("Calculate the users average")
     public void getAllGrades_andCalcAverage() {
         // Create fake data
         User firstUser = new User("1", "Yehia", 50);
@@ -43,5 +48,14 @@ public class UserServiceTest {
         float average = 62.5F;
         assertThat(userService.calcAverage()).isEqualTo(average);
     }
+    @Test
+    @DisplayName("Calculate the users average when there's no data ")
+    public void getAllGrades_andCalcAverageWhenUsersAreNull()
+    {
+        List<User> users = new ArrayList<>();
+        given(userRepository.findAll()).willReturn(users);
+        assertThat(userService.calcAverage()).isEqualTo(0f);
+    }
+
 
 }
